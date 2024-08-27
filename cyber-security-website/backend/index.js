a77ebc75a3841439c7e80e4bb9c09cc1c093ffc7
Likhith Raj V <87167313+ItsmeLikhithRajV@users.noreply.github.com>
2024-08-27 23:33:53 +0530

    index.js

--- cyber-security-website/backend/index.js
+++ cyber-security-website/backend/index.js
@@ -7,7 +7,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-const NEWS_API_KEY = '4547ad67515b4bbc9eb957c3cdb95622'; // Replace with your News API key
+const NEWS_API_KEY = '4547ad67515b4bbc9eb957c3cdb95622';
 const NEWS_API_URL = `https://newsapi.org/v2/top-headlines?country=us&category=technology&q=cybersecurity OR cybercrime&apiKey=${NEWS_API_KEY}`;
 
 app.get('/api/news', async (req, res) => {
@@ -17,7 +17,7 @@ app.get('/api/news', async (req, res) => {
       title: article.title,
       description: article.description,
       url: article.url,
-      urlToImage: article.urlToImage // Ensure this field is included
+      urlToImage: article.urlToImage
     }));
     res.json(articles);
   } catch (error) {
